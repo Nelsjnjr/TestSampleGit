6d3401f7875dacf4cb345174dbcf5728d4abc03d
NRaj.Techmantra <NRaj.Techmantra@EMRITDT0094>
2018-01-22 09:54:28 +0400

    Add project files.

---
+++ TestSampleGit/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace TestSampleGit.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ TestSampleGit/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestSampleGit.Startup))]
+namespace TestSampleGit
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
